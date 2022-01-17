ced7dd4b7079d2c51d7ab45ec079c060fa0970aa
Patricia <pmaceirasvarela@danielcastelao.org>
2022-01-17 10:55:44 +0100

    Añado localizacion con permisos

--- app/src/main/java/com/dam2a/realtime/MapsActivity.kt
+++ app/src/main/java/com/dam2a/realtime/MapsActivity.kt
@@ -1,7 +1,14 @@
 package com.dam2a.realtime
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,11 +18,14 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.dam2a.realtime.databinding.ActivityMapsBinding
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
+GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
-
+    companion object {
+        const val REQUEST_CODE_LOCATION = 0
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -44,5 +54,124 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+    private fun createMarker() {
+        val vigo = LatLng(42.23282, -8.72264)
+        mMap.addMarker(MarkerOptions().position(vigo).title("Vigo"))
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(vigo, 18f),
+            4000,
+            null
+        )
+    }
+
+    /**
+     * Método que comprueba que el permiso este activado, pidiendo el permiso y viendo si es igual a el PERMISSION_GRANTED
+     */
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+
+    //SupressLint es una interfaz que indica que se deben ignoar las advertencias especificadas
+
+    /**
+     * Método que intenta activar la localización
+     */
+    @SuppressLint("MissingPermission")
+    private fun enableLocation() {
+        //Si el mapa no está inicializado vete
+        if (!::mMap.isInitialized) return
+        if (isLocationPermissionGranted()) {
+            //si  ha aceptado los permisos permitimos la localización
+            mMap.isMyLocationEnabled = true
+        } else {
+            //no está activado el permiso , se lo tenemos que pedir a través
+            // del siguiente método
+            requestLocationPermission()
+        }
+    }
+
+    /**
+     * Método para pedir al usuario que active los permisos
+     */
+    private fun requestLocationPermission() {
+        //Si cumple el primer if significa que le hemos pedido ya al usuario los permisos y los ha rechazado
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            Toast.makeText(this, "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+
+        } else {
+            //Si entra por el else es la primera vez que le pedimos los permisos
+            //Se le pasa el companion object para saber si ha aceptado los permisos
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION
+            )
+        }
+    }
+
+    /**
+     * Método que captura la respuesta del usuario si acepta los permisos
+     */
+    @SuppressLint("MissingSuperCall", "MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        // cuando el requestcode se igual al companion object definido
+        when (requestCode) {
+            // Si grantResults no está vacio y el permiso 0 está aceptado signfica que ha acptado nuestros permisos
+            REQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mMap.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(
+                    this,
+                    "Para activar la localización ve a ajustes y acepta los permisos",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+            //Por si ha aceptado otro permiso
+            else -> {
+            }
+        }
+    }
+
+    /**
+     * Método que sirve para comprobar si los permisos siguen activos caundo el usuario
+     * se va de la aplicación y vuelve para que la aplicación no rompa
+     */
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (!::mMap.isInitialized) return
+        if (!isLocationPermissionGranted()) {
+            !mMap.isMyLocationEnabled
+            Toast.makeText(this, "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+
+
+        }
+    }
+
+    /**
+     * Método que sirve para que cuando el usuario pulse el botón OnMyLocation
+     * le lleve a la ubicación
+     */
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "Boton pulsado", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    /**
+     * Método que muestra la latitud y longitud de nuestra ubicación cuando pulsamos sobre ella
+     */
+    override fun onMyLocationClick(p0: Location) {
+        Toast.makeText(this, "Estás en ${p0.latitude},${p0.longitude} ", Toast.LENGTH_SHORT).show()
     }
 }
